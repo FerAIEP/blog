4080d8d19b9e0e9505a4500ad41895377eac7a5f
FerAIEP <aiep.bootcamp@gmail.com>
2021-08-26 09:53:39 -0300

    agregando rediccionamiento a show y seguridad en parametros

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save
+
+    redirect_to @article # es igual que poner redirect_to article_path(@article)
+  end
+
+private
+  
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
