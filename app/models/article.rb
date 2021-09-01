2f66119eea227aa9ce65c22ddade64b94c21ab18
FerAIEP <aiep.bootcamp@gmail.com>
2021-09-01 10:22:02 -0300

    refactorizando vistas con render y prueba callback before_save

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates(:title,{ presence: true, length: { minimum: 5 } })
+  before_save :mayuscula
+
+  def mayuscula
+    self.title = self.title.upcase
+  end
 end
