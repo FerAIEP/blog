c1c0ce7935bbe7bdea5b5742ee78fd6686cd1adc
FerAIEP <aiep.bootcamp@gmail.com>
2021-09-03 10:56:42 -0300

    rails seeds(datos fake en la DDBB)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# puts "eliminando articulos..."
+# Article.destroy_all
+puts "creando articulos..."
+articles = Article.create([
+  {title: "Primer articulo del viernes", text: "este es el texto del día viernes"},
+  {title: "Segundo articulo del viernes", text: "este es el texto del día viernes"},
+  {title: "Tercero articulo del viernes", text: "este es el texto del día viernes"}
+])
+puts "articulos creados exitosamente..."
+puts "creando comentarios..."
+Comment.create(commenter: "Este es un comentario nuevo", body: "body del comentario", article: articles.first)
+puts "comentarios creados"
